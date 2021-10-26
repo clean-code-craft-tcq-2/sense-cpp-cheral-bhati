e178239d7f5229c22d67ab6220ec270fe685cdda
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2021-10-26 08:43:27 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,4 +12,32 @@ namespace Statistics
     Stats ComputeStatistics(const std::vector<float>& );
     }
     
+struct EmailAlert 
+{
+    bool emailSent;
+};
+struct LEDAlert 
+{
+    bool ledGlows;
+};
+struct IAlerter
+{
+    EmailAlert *emailAlertPtr;
+    LEDAlert *ledAlertPtr;
+};
+
+class StatsAlerter
+{
+    float maxThreshold;
+    std::vector<IAlerter*>alerters;
+
+public:
+    StatsAlerter(const float Threshold,  std::vector<IAlerter*> Initialv )
+    {
+        maxThreshold =  Threshold;
+        alerters     = Initialv;
+    }
+    
+    void checkAndAlert( const std::vector<float>& );
+};
    
